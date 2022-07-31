6ce7b8b26c75d5908b0486810c16df6400bb7932
Alesio López <alesiolopez26@gmail.com>
2022-07-31 19:37:17 -0300

    Se actualizaron unos alert

--- js/script.js
+++ js/script.js
@@ -2,27 +2,31 @@ function Programa(){
     alert("A continuación, elegir una de las siguientes opciones. Clic en 'Aceptar'.")
     let opcion = prompt("1) Sumar. 2) Restar. 3) Multiplicar. 4) Dividir.")
 
-    if(opcion == 1){
+if(opcion == 1){
         let num1 = parseInt(prompt("Ingresar primer número."));
         let num2 = parseInt(prompt("Ingresar segundo número"));
         let suma = num1+num2;
-        alert("La suma de " + num1 + " y " + num2 + " es de: " + suma + ".");
+        alert("Resultado: " + num1 + " + " + num2 + " = " + suma);
+        alert("¡Gracias por utilizar el programa! =)")
     }else if(opcion == 2){
         let num1 = parseInt(prompt("Ingresar primer número."));
         let num2 = parseInt(prompt("Ingresar segundo número"));
         let resta = num1 - num2;
-        alert("La resta entre " + num1 + " y " + num2 + " es de: " + resta + ".");
+        alert("Resta: " + num1 + " - " + num2 + " = " + resta);
+        alert("¡Gracias por utilizar el programa! =)")
     }else if(opcion == 3){
         let num1 = parseInt(prompt("Ingresar primer número."));
         let num2 = parseInt(prompt("Ingresar segundo número"));
         let multiplicar = num1 * num2;
-        alert("La multiplicación entre " + num1 + " y " + num2 + " es de: " + multiplicar + ".");
+        alert("Multiplicación: " + num1 + " x " + num2 + " = " + multiplicar);
+        alert("¡Gracias por utilizar el programa! =)")
     }else if(opcion == 4){
         let num1 = parseInt(prompt("Ingresar primer número."));
         let num2 = parseInt(prompt("Ingresar segundo número"));
         let dividir = num1/num2;
-        alert("La división entre " + num1 + " y " + num2 + " es de: " + dividir + ".");
+        alert("División: " + num1 + " / " + num2 + " = " + dividir);
+        alert("¡Gracias por utilizar el programa! =)")
     } else {
-        alert("Opción no encontrada");
+        alert("Opción no encontrada, reintentar. ;-)");
     }
 }
