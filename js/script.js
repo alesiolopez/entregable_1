2553d7d12974abde2591aa7e9629c9537f55f6fa
Alesio López <alesiolopez26@gmail.com>
2022-07-31 19:17:35 -0300

    Se realizó modificiación total del programa.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 function Programa(){
-    let num = prompt("Ingresar un numero");
-    let palabra = prompt("Ingresar palabra para repetir");
+    alert("A continuación, elegir una de las siguientes opciones. Clic en 'Aceptar'.")
+    let opcion = prompt("1) Sumar. 2) Restar. 3) Multiplicar. 4) Dividir.")
 
-    for (let i = 0; i < num; i++) {
-    alert("Palabra que se va a repetir: " + palabra + ". La cantidad de veces " + (i + 1) + "/" + num);
+    if(opcion == 1){
+        let num1 = parseInt(prompt("Ingresar primer número."));
+        let num2 = parseInt(prompt("Ingresar segundo número"));
+        let suma = num1+num2;
+        alert("La suma de " + num1 + " y " + num2 + " es de: " + suma + ".");
+    }else if(opcion == 2){
+        let num1 = parseInt(prompt("Ingresar primer número."));
+        let num2 = parseInt(prompt("Ingresar segundo número"));
+        let resta = num1 - num2;
+        alert("La resta entre " + num1 + " y " + num2 + " es de: " + resta + ".");
+    }else if(opcion == 3){
+        let num1 = parseInt(prompt("Ingresar primer número."));
+        let num2 = parseInt(prompt("Ingresar segundo número"));
+        let multiplicar = num1 * num2;
+        alert("La multiplicación entre " + num1 + " y " + num2 + " es de: " + multiplicar + ".");
+    }else if(opcion == 4){
+        let num1 = parseInt(prompt("Ingresar primer número."));
+        let num2 = parseInt(prompt("Ingresar segundo número"));
+        let dividir = num1/num2;
+        alert("La división entre " + num1 + " y " + num2 + " es de: " + dividir + ".");
+    } else {
+        alert("Opción no encontrada");
     }
-
-    alert("Programa terminado.");
 }
