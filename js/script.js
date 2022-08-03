a01021af78a240c7ea5f33587c7e46016da10ba1
Alesio López <alesiolopez26@gmail.com>
2022-08-03 13:49:10 -0300

    modificando unas definiciones del script.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function Programa(){
     alert("Ejecutando programa.")
-    let accion = prompt("Dime qué hacer");
+    let accion = prompt("Dime qué hacer: Saltar, Correr o Comer.");
     let repeticion = prompt("Ingresar la cantidad de veces que quiere realizar la acción. Entre 1 y 10.")
 
     if(repeticion > 0 && repeticion <= 10){
@@ -8,10 +8,12 @@ function Programa(){
             for (let i = 0; i < repeticion; i++) {
                 alert(accion + ". Cantidad de veces: " + (i+1) + " de " + repeticion);
             }
-        } else if(repeticion < 10){
+        } else if(repeticion <= 10){
             for (let i = 0; i < repeticion; i++) {
                 alert("Súper " + accion + "!. Cantidad de veces: " + (i+1) + " de " + repeticion);
             }
-        }
+        } 
+    }else{
+        alert("Ingresa la cantidad entre 1 y 10. Vuelve a intentar rey.");
     }
 }
