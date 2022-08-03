15cbd4c11840ddb4c6ddc7680142c015a965da6d
Alesio López <alesiolopez26@gmail.com>
2022-08-03 14:21:37 -0300

    Agregué un ingreso mas para saber la etapa de la persona

--- js/script.js
+++ js/script.js
@@ -16,4 +16,21 @@ function Programa(){
     }else{
         alert("Ingresa la cantidad entre 1 y 10. Vuelve a intentar rey.");
     }
+
+    let edad = prompt("Ingresa ahora tu edad para saber en qué etapa estas.");
+
+    if(edad >= 0 && edad <= 5){
+        alert("Estas en la etapa de Primera Infancia");
+    } else if (edad >= 6 && edad <= 11){
+        alert("Estas en la etapa de Infancia");
+    } else if(edad >= 12 && edad < 18){
+        alert("Estas en la etapa de Adolescencia");
+    } else if(edad >= 18 && edad <= 26){
+        alert("Estas en la etapa de Juventud");
+    } else if(edad >= 27 && edad <= 59){
+        alert("Estas en la etapa de Adultez");
+    }else if(edad >= 60){
+        alert("Estas en la etapa de Persona Mayor");
+    }
+alert("Gracas por utilizar le programa rey.")
 }
